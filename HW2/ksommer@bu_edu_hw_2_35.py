fcc152d77b875118c97cb283ce22cab9f22219d8
Karenommer <ksommerh94@gmail.com>
2021-02-06 22:10:21 -0500

    finish

--- HW2/ksommer@bu_edu_hw_2_35.py
+++ HW2/ksommer@bu_edu_hw_2_35.py
@@ -6,19 +6,28 @@ CS 521 Spring 2021
 
 Assignment 2
 
-Problem 33 and 78
+Problem 35 and 78
 
 '''
 
-TODOOO
-#Consider a triangle with sides of length 3, 7, and 9.
-#The law of cosines states that given three sides of a triangle (a, b, and c)
-#and the angle C between sides a and b: c**2 = a**2 + b**2 − 2*a*b*cos(C ).
-#Write Python code to calculate the three angles in the triangle.
+#Consider a triang_le with sides of length 3, 7, and 9.
+#The law of cosines states that given three sides of a triang_le (a, b, and c)
+#and the ang_le C between sides a and b: c**2 = a**2 + b**2 − 2*a*b*cos(C ).
+#Write Python code to calculate the three ang_les in the triang_le.
 
-s1=3
-s2=7
-s3=9
-#angles
+import math
+a=3
+b=7
+c=9
 
-print()
+ang_A = math.degrees(math.acos((c**2 - b**2 - a**2)/(-2.0 * a * b)))
+ang_B = math.degrees(math.acos((a**2 - c**2 - b**2)/(-2.0 * b * c)))
+ang_C = math.degrees(math.acos((b**2 - a**2 - c**2)/(-2.0 * c * a)))
+
+
+check=ang_A+ang_B+ang_C
+#check if sum of angles is 180
+#print(check)
+print(ang_A)
+print(ang_B)
+print(ang_C)
--- HW2/ksommer@bu_edu_hw_2_45.py
+++ HW2/ksommer@bu_edu_hw_2_45.py
@@ -6,11 +6,10 @@ CS 521 Spring 2021
 
 Assignment 2
 
-Problem 41 and 78
+Problem 45 and 78
 
 
 '''
-TODOOOO
 
 
 #The world record in the 100-meter dash is 9.58 seconds, set by Usain Bolt.
@@ -20,5 +19,18 @@ TODOOOO
 #calculate and print the runner’s average speed in miles per hour.
 
 
-ft=5280
-m=1
+
+print('Enter time of runner for 100 meters in seconds')
+input_time = float(input())
+#convert mts to miles - distance
+mts_100_in_ft=328
+ft_to_mile=mts_100_in_ft/5280
+#print(ft_to_mile)
+
+#convert seconds to hour - time
+hour=input_time/3600
+#print(hour)
+
+#Avg speed=total distance/time
+avg_speed=ft_to_mile/hour
+print('The runner’s average speed in miles per hour is ',round(avg_speed,2), 'mph')
