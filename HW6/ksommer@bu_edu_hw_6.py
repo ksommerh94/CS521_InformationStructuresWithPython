13a00e58ed100754bb0b47200c302f55352fa590
ksommerh94 <ksommerh94@gmail.com>
2021-03-06 12:50:58 -0500

    .

--- HW6/ksommer@bu_edu_hw_6.py
+++ HW6/ksommer@bu_edu_hw_6.py
@@ -10,26 +10,29 @@ Created on Wed Mar  3 08:54:01 2021
 
 text = """Angels and
 demons is book
-about illuminari 
+about illuminati
 today"""
 
 cursor = 4
 x_list = list(text)
 
-def print_file(x, cur, sep="$"):
+def print_file(x, cur, sep="|"):
     print(x[:cur] + sep + x[cur:] + "\n" + "-----")
-    
-def cmd_h(x, cur):   # move cursor to the left 
+
+# move cursor to the left
+def cmd_h(x, cur):
     if cur > 0:
         cur = cur -1
     return x, cur
-    
-def cmd_I(x, cur):     # move to the right
+
+# move to the right
+def cmd_I(x, cur):
     if cur < len(x) -1:
         cur = cur + 1
     return x, cur
 
-def cmd_X(x, cur): # delete char to the left of the cursos
+# delete char to the left of the cursos
+def cmd_X(x, cur):
     if cur > 0:
         left = x[ : cur-1]
         right = x[cur : ]
@@ -37,39 +40,41 @@ def cmd_X(x, cur): # delete char to the left of the cursos
         x = left + right
     return x, cur
 
-
-def cmd_x(x, cur):   # delete char to right of cursor
+# delete char to right of cursor
+def cmd_x(x, cur):
     if cur < len(x) - 1:
         x =  x[ : cur]  + x[cur+1 : ]
     return x, cur
 
-
-def cmd_n(x, cur, target):    # find first occurence of target after cursor
+# find first occurence of target after cursor
+def cmd_n(x, cur, target):
     pos = x.find(target, cur, len(x))
     if pos >= 0:
         cur = pos
     return x, cur
 
-
-def cmd_a(x, cur, target): # append target after cursor
+# append target after cursor
+def cmd_a(x, cur, target):
     x = x[ : cur] + target + x[cur : ]
     return x, cur
-    
-def cmd_r(x, cur, new_char):    # replace character at cursor
+
+# replace character at cursor
+def cmd_r(x, cur, new_char):
     if cur < len(x) - 1:
         x = x[ : cur] + new_char + x[cur+1 : ]
     return x, cur
-        
-        
-def cmd_D(x, cur):  # remove from cur to end of line
+
+# remove from cur to end of line
+def cmd_D(x, cur):
         pos = x.find("\n",  cur, len(x))
         if pos >=0 :
             x = x[ : cur] + x[pos : ]
         else:
             x = x[ : cur]
         return x, cur
-    
-def cmd_j(x, cur):     # move vertically up  
+
+# move vertically up
+def cmd_j(x, cur):
     a = x.rfind("\n", 0, cur)
     if a >0 :
         delta = cur - a
@@ -80,72 +85,65 @@ def cmd_j(x, cur):     # move vertically up
         else:
             cur = a - 1
     return x, cur
-        
-        
-    
-    
+
+
+
+
 
 """
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_I(text, cursor)
-print_file(text, cursor, "$")
-   
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
+
+print_file(text, cursor, "|")
 text, cursor = cmd_h(text, cursor)
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 cursor = 0
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_h(text, cursor)
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 
 cursor = 0
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_X(text, cursor)
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 """
 """
 cursor = 4
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_n(text, cursor, "book")
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 text, cursor = cmd_n(text, cursor, "Vatican")
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 """
 
 cursor = 4
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_n(text, cursor, "book")
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 text, cursor = cmd_a(text, cursor , "nice ")
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 
 text, cursor = cmd_r(text, cursor , "N")
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 text, cursor = cmd_x(text, cursor )
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 text, cursor = cmd_D(text, cursor)
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 
 cursor = 43
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_D(text, cursor)
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 
 cursor = 35
-print_file(text, cursor, "$")
+print_file(text, cursor, "|")
 text, cursor = cmd_j(text, cursor)
 print_file(text, cursor)
-
-
-
-
-
-
-
